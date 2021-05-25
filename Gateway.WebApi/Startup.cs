7be572402ee52271a45253a45cf63aa310aedd1c
Shahrzad jahanbaz <shahrzadjahanbaz@gmail.com>
2021-05-25 22:49:04 +0430

    Update Gateway.Api startup.cs

--- Gateway.WebApi/Startup.cs
+++ Gateway.WebApi/Startup.cs
@@ -23,37 +23,27 @@ namespace Gateway.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy(name: MyAllowSpecificOrigins,
-                    builder =>
-                    {
-                        builder.AllowAnyHeader();
-                        builder.AllowAnyOrigin();
-                        builder.AllowAnyMethod();
-
-                    });
-            });
+            services.AddCors();
             services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(MyAllowSpecificOrigins);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Gateaway is running ... ");
                 });
             });
 
